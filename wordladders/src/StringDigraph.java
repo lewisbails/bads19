a44893a30c0a0dc5fd768de5edeec708736ac51a
lewisbails <lbai@oticon.com>
2019-04-03 14:05:11 +0200

    stupid java not being python

--- wordladders/src/StringDigraph.java
+++ wordladders/src/StringDigraph.java
@@ -23,7 +23,7 @@ public class StringDigraph {
             if (E < 0) throw new IllegalArgumentException("number of edges in a Digraph must be nonnegative");
             for (int i = 0; i < E; i++) {
                 String v = in.readString();
-                String lastfour = new String(Insertion.sort(v.substring(1).split('')));
+                String lastfour = new String(Insertion.sort(v.substring(1).split("")));
                 if (!graph.contains(v)){
                 	graph.put(v,new Bag<>());
                 	for (String combination : fourcombos(v)){
@@ -45,7 +45,7 @@ public class StringDigraph {
 
 
     private String[] fourcombos(String word){
-    	String[] chars = Insertion.sort(word.split(''));
+    	String[] chars = Insertion.sort(word.split(""));
     	String[] combos = new String[5];
     	combos[0] = ""+chars[0]+chars[1]+chars[3]+chars[4];
     	combos[1] = ""+chars[0]+chars[1]+chars[2]+chars[3];
